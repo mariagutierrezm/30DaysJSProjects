df8b3edb1d555256bc9f8c7dcf57f7ad062d1a18
Maria <gutierrez.mari90@gmail.com>
2020-08-14 11:49:51 +0100

    added new functions and remove functionality

--- LS-ShoppingList/shopping.js
+++ LS-ShoppingList/shopping.js
@@ -5,20 +5,22 @@
   let done = false; 
 
   function getLocalStorage(){
-    return localStorage.getItem("itemsList")
-    ? JSON.parse(localStorage.getItem("itemsList"))
+    return localStorage.getItem('itemsList')
+    ? JSON.parse(localStorage.getItem('itemsList'))
     : [];
   }  
 
-  function createElement(i, text) {
+  function createElement(id, text) {
     let items = getLocalStorage();
     const element = document.createElement("li");
     element.innerHTML = `
-            <input type="checkbox" data-index="${i}" id="item${i}" ${items.done ? 'checked' : ''} />
-            <label for="item${i}" class="ingredients_done">${text}</label> 
-            <a class="ingredients_close" href="${i}">x</a> 
+            <input type="checkbox" id="item${id}" ${items.done ? 'checked' : ''} />
+            <label for="item${id}" class="ingredients_done">${text}</label> 
+            <a class="ingredients_close" href="${id}">x</a> 
         `;
-    itemsList.appendChild(element);   
+    itemsList.appendChild(element); 
+    const deleteBtn = element.querySelector('.ingredients_close');
+    deleteBtn.addEventListener("click", removeItem);  
   }
   
   function setUpItem() {
@@ -26,14 +28,14 @@
 
     if(items.length > 0) {
       items.forEach(function (item) {
-        createElement(item.i, item.text);
+        createElement(item.id, item.text);
       });
     }
   }
 
-  function addItemToLocalStorage(i, text) {
+  function addItemToLocalStorage(id, text) {
       const item = {
-        i,
+        id,
         text,
         done
       };
@@ -47,31 +49,75 @@
     e.preventDefault();
     let items = getLocalStorage();
     const text = inputText.value;
-    const i = items.length;
-    createElement(i, text);
-    addItemToLocalStorage(i, text);
+    const id = getUniqueID();
+    createElement(id, text);
+    addItemToLocalStorage(id, text);
     this.reset();
   }
 
-  
-  $('.ingredients').on('click', 'li a', function(event){
-      event.preventDefault();
-      $( this ).parent().hide( 400 );
-      const index = event.target.getAttribute('href');
-      console.log(index);
-      removeItemfromArray(index);
-  });
+  function getUniqueID(){
+    // always start with a letter (for DOM friendlyness)
+    var idstr=String.fromCharCode(Math.floor((Math.random()*25)+65));
+    do {                
+        // between numbers and characters (48 is 0 and 90 is Z (42-48 = 90)
+        var ascicode=Math.floor((Math.random()*42)+48);
+        if (ascicode<58 || ascicode>64){
+            // exclude all chars between : (58) and @ (64)
+            idstr+=String.fromCharCode(ascicode);    
+        }                
+    } while (idstr.length<32);
 
-  function removeItemfromArray(index){ 
+    return (idstr);
+}
+  
+  // function findIndex(array, value){
+  //   var index = 0;
+  //   for(var i = 0; i < array.length; i++) {
+  //     if(array[i].id === value){
+  //       index = i;
+  //     }
+  //   }
+  //   index;
+  // }
+
+  function removeItem(e){
+    e.preventDefault(); 
+    const element = e.target.parentElement;
+    itemsList.removeChild(element);
+
+    const idElement = e.target.getAttribute('href');
     let items = getLocalStorage();
-    let newArr = items.splice(index, 1);
-    localStorage.setItem('items', JSON.stringify(items)); 
+    // findIndex(items, idElement);
+    var index = 0;
+    for(var i = 0; i < items.length; i++) {
+      if(items[i].id === idElement){
+        index = i;
+      }
+    }
+    console.log(index);
+    // var index = items.map(item => item.i).indexOf(element);
+    // var index = items.indexOf(item => item.element === i);
+    
+    removeFromLocalStorage(index)
+    /* https://stackoverflow.com/questions/7176908/how-to-get-index-of-object-by-its-property-in-javascript/54015295
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+    https://www.youtube.com/watch?v=etLF49Ete-0
+    https://www.youtube.com/watch?v=lPZZwkrJ5n0&t=162s
+    */
   };
 
+  function removeFromLocalStorage(i) {
+    let items = getLocalStorage();
+    let newArr = items.splice(i, 1);
+    localStorage.setItem('itemsList', JSON.stringify(items)); 
+  }
+
   function toggleDone(e) {
-    if (e.target.matches('a')) {
-      console.log('clicked');
-    } else if (e.target.matches('input')) {
+    // if (e.target.matches('a')) {
+    //   console.log('clicked');
+    // } else 
+    if (e.target.matches('input')) {
       const element = e.target;
       const index = element.dataset.index;
       let items = getLocalStorage();
@@ -84,7 +130,7 @@
   
   addItems.addEventListener('submit', addItemByEvent);
   itemsList.addEventListener('click', toggleDone);
-  window.addEventListener("DOMContentLoaded", setUpItem);
+  window.addEventListener('DOMContentLoaded', setUpItem);
 //TODO- seach how to  //how do I update that one list item that I have instead of rendering the entire list - REACT or Angular
 // instead of rerender the entire list 
 //TODO-make a button uncheck all, check all, and mirror it all to localStorage
